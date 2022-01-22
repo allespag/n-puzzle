615c618a2f317e89403c332c99d84a9fd38f8076
Allespag <allespag@student.42.fr>
2022-01-22 16:37:40 +0100

    fixed manhattan

--- npuzzle/distance.py
+++ npuzzle/distance.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from typing import Protocol, Type
 
-from npuzzle.npuzzle import Npuzzle
-from npuzzle.utils import index_to_coor
+from npuzzle.npuzzle import EMPTY_TILE, Npuzzle
+from npuzzle.utils import coor_to_index, index_to_coor
 
 
 class Distance(Protocol):
@@ -15,6 +15,8 @@ class Manhattan:
     def compute(self, src: Npuzzle, dst: Npuzzle) -> int:
         distance = 0
         for i, tile in enumerate(src.tiles):
+            if tile == EMPTY_TILE:
+                continue
             dst_index = dst.tiles.index(tile)
             src_x, src_y = index_to_coor(i, (src.n, src.n))
             dst_x, dst_y = index_to_coor(dst_index, (dst.n, dst.n))
@@ -26,6 +28,25 @@ class Manhattan:
 class TilesOutOfPlace:
     def compute(self, src: Npuzzle, dst: Npuzzle) -> int:
         return sum(a != b for a, b in zip(src.tiles, dst.tiles))
+
+
+class LinearConflict:
+    def compute(self, src: Npuzzle, dst: Npuzzle) -> int:
+        conflict = 0
+
+        # for i in range(src.n):
+        #     src_row = src.tiles[i * src.n : (i + 1) * src.n]
+        #     src_col = src.tiles[i :: src.n]
+        #     dst_row = dst.tiles[i * src.n : (i + 1) * src.n]
+        #     dst_col = dst.tiles[i :: src.n]
+
+        #     for x, y in zip(src_row, dst_row):
+        #         if x == 0 or x == y:
+        #             continue
+        #         if x in dst_row:
+        #             for i
+
+        return Manhattan().compute(src, dst) + 2 * conflict
 
 
 AVAILABLE_HEURISTICS: list[Type[Distance]] = [
