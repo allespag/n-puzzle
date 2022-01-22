022fe2a1458583e5fd9ee11999bfa1aa4e00b556
Allespag <allespag@student.42.fr>
2022-01-22 19:37:20 +0100

    working on linear conflict

--- npuzzle/distance.py
+++ npuzzle/distance.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Protocol, Type
 
 from npuzzle.npuzzle import EMPTY_TILE, Npuzzle
-from npuzzle.utils import coor_to_index, index_to_coor
+from npuzzle.utils import index_to_coor
 
 
 class Distance(Protocol):
@@ -31,20 +31,36 @@ class TilesOutOfPlace:
 
 
 class LinearConflict:
-    def compute(self, src: Npuzzle, dst: Npuzzle) -> int:
+    def __count_conflict(self, src_row: list[int], dst_row: list[int]) -> int:
         conflict = 0
 
-        # for i in range(src.n):
-        #     src_row = src.tiles[i * src.n : (i + 1) * src.n]
-        #     src_col = src.tiles[i :: src.n]
-        #     dst_row = dst.tiles[i * src.n : (i + 1) * src.n]
-        #     dst_col = dst.tiles[i :: src.n]
+        for j in range(len(src_row)):
+            if src_row[j] == EMPTY_TILE or src_row[j] not in dst_row:
+                continue
+            for k in range(j + 1, len(src_row)):
+                if src_row[k] == EMPTY_TILE or src_row[k] not in dst_row:
+                    continue
+                goal_k_index = dst_row.index(src_row[k])
+                if j == goal_k_index:
+                    conflict += 1
 
-        #     for x, y in zip(src_row, dst_row):
-        #         if x == 0 or x == y:
-        #             continue
-        #         if x in dst_row:
-        #             for i
+        return conflict
+
+    def compute(self, src: Npuzzle, dst: Npuzzle) -> int:
+        src_rows: list[list[int]] = []
+        dst_rows: list[list[int]] = []
+        src_cols: list[list[int]] = []
+        dst_cols: list[list[int]] = []
+        for i in range(src.n):
+            src_rows.append(src.tiles[i * src.n : (i + 1) * src.n])
+            src_cols.append(src.tiles[i :: src.n])
+            dst_rows.append(dst.tiles[i * src.n : (i + 1) * src.n])
+            dst_cols.append(dst.tiles[i :: src.n])
+
+        conflict = 0
+        for i in range(src.n):
+            conflict += self.__count_conflict(src_rows[i], dst_rows[i])
+            conflict += self.__count_conflict(src_cols[i], dst_cols[i])
 
         return Manhattan().compute(src, dst) + 2 * conflict
 
@@ -52,6 +68,7 @@ class LinearConflict:
 AVAILABLE_HEURISTICS: list[Type[Distance]] = [
     Manhattan,
     TilesOutOfPlace,
+    LinearConflict,
 ]
 
 DEFAULT_HEURISTIC: Type[Distance] = Manhattan
