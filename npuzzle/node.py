83e54159a0a46aef95728558404f65c039e13a58
Allespag <allespag@student.42.fr>
2022-01-24 17:36:34 +0100

    removed f as attr

--- npuzzle/node.py
+++ npuzzle/node.py
@@ -8,9 +8,8 @@ COST = 1
 class Node:
     def __init__(self, state: Npuzzle) -> None:
         self.state = state
-        # self.f = 0  # total cost of the node
-        self.g = 0  # distance between the current node and the start node
-        self.h = 0  # estimated distance from the current node to the end node
+        self.g = 0
+        self.h = 0
         self.parent: Node | None = None
 
     @property
